e1af23ec27a59f3b3ab021232c156d7423396f2c
JosephGabriells <rm92920@fiap.com.br>
2022-05-11 14:47:23 -0300

    Exercicio 3

--- js/script.js
+++ js/script.js
@@ -49,9 +49,28 @@ tabuada.addEventListener('click',function(){
         let resultado = numero * i
         console.log(numero + "*" + i + "=" + resultado)
      }
-
-  
 })
 
 
+// Exercicio 3
 
+const repeticao = document.querySelector("#calcularRepeticao")
+
+repeticao.addEventListener('click',function(){
+    let vetor = ["1", "2", "1", "1", "3", "2", "3", "5", "5", "8", "8", "4"]
+    vetor.sort()
+    let odd = 0
+
+
+    for (let i = 0; i < vetor.length; i++) {
+       let atual = vetor[i]
+        for (let j = 1; j < vetor.length; j++){
+            let proximo = vetor[j]
+            if (atual != proximo){
+                odd = proximo
+            }
+        }
+     }
+
+     console.log("Odd " + odd)
+})
